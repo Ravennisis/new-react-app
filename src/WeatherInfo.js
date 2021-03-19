f47dce37f69d8678f649b86bd8c0a6e3193c9ec7
Ravennisis <76167228+Ravennisis@users.noreply.github.com>
2021-03-19 15:20:31 +0100

    added weather icon (able to change with weather description)

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
@@ -16,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-            
+            <WeatherIcon code={props.data.icon} />
             </div>
 
             <div className="float-left">
